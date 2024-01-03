3406c96572c035b8ed014fe3836cf6fc5fcaa77a
Arash Sahebolamri <arash.sahebolamri@fortanix.com>
2024-01-03 11:24:31 -0800

    Test FFDHE support against OpenSSL

--- ci-tests/src/lib.rs
+++ ci-tests/src/lib.rs
@@ -1,2 +1,6 @@
-#[cfg(test)]
+#![cfg(test)]
+
+mod ffdhe;
+mod test_ffdhe_kx_against_openssl;
+mod utils;
 mod validate_ffdhe_params;
--- ci-tests/src/validate_ffdhe_params.rs
+++ ci-tests/src/validate_ffdhe_params.rs
@@ -1,47 +1,7 @@
 use base64::prelude::*;
 use rustls::{ffdhe_groups::FfdheGroup, NamedGroup};
 
-/// If OpenSSL 3 is not avaialble, panics with a helpful message
-fn verify_openssl3_available() {
-    let openssl_output = std::process::Command::new("openssl")
-        .args(["version"])
-        .output();
-    match openssl_output {
-        Ok(output) => {
-            if !output.status.success() {
-                panic!(
-                    "OpenSSL exited with an error status: {}\n{}",
-                    output.status,
-                    std::str::from_utf8(&output.stderr).unwrap_or_default()
-                );
-            }
-            let version_str = std::str::from_utf8(&output.stdout).unwrap();
-            let parts = version_str
-                .split(' ')
-                .collect::<Vec<_>>();
-            assert_eq!(
-                parts.first().copied(),
-                Some("OpenSSL"),
-                "Unknown version response from OpenSSL: {version_str}"
-            );
-            let version = parts.get(1);
-            let major_version = version
-                .and_then(|v| v.split('.').next())
-                .unwrap_or_else(|| {
-                    panic!("Unexpected version response from OpenSSL: {version_str}")
-                });
-            assert!(
-                major_version
-                    .parse::<usize>()
-                    .is_ok_and(|v| v >= 3),
-                "OpenSSL 3+ is required for the tests here. The installed version is {version:?}"
-            );
-        }
-        Err(e) => {
-            panic!("OpenSSL 3+ needs to be installed and in PATH.\nThe error encountered: {e}")
-        }
-    }
-}
+use crate::utils::verify_openssl3_available;
 
 /// Get FFDHE parameters `(p, g)`` for the given `ffdhe_group` from OpenSSL
 fn get_ffdhe_params_from_openssl(ffdhe_group: NamedGroup) -> (Vec<u8>, Vec<u8>) {
